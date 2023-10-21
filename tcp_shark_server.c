452380fd24fbcf1360a26a2660e4f6bf1b24d777
LiYu <71813593+LiYuMax@users.noreply.github.com>
2023-10-21 11:09:14 +0800

    Update tcp_shark_server.c

--- tcp_shark_server.c
+++ tcp_shark_server.c
@@ -3,7 +3,8 @@
 #include <sys/select.h>
 #include <time.h>
 #include <sqlite3.h>
-typedef struct mseeage{
+typedef struct mseeage
+{
     char option;
     char name[32];
     char text[256];
@@ -16,17 +17,16 @@ int do_record(int sockfd,msg_t *msg,sqlite3 *db);
 msg_t msg;
 int num;
 int money;
-int login_func(void *arg,int columns,char **f_value,char **f_name){
+int login_func(void *arg,int columns,char **f_value,char **f_name)
+{
 	num++;
 	money=atoi(f_value[2]);
 	return 0;
 }
-int record_func(void *arg,int columns,char **f_value,char **f_name){
-	printf("我是记录函数\n");
+int record_func(void *arg,int columns,char **f_value,char **f_name)
+{
 	int sockfd=*(int *)arg;
 	msg_t msg;
-	//注意这里加*会报错
-	//sprintf(msg.text,"[%s--%s]",*f_value[0],*f_value[1]);
 	int num=atoi(f_value[2]);
 	if(0<num)
 	sprintf(msg.text,"您于[%s]存入[%s]",f_value[1],f_value[2]);
@@ -38,13 +38,15 @@ int record_func(void *arg,int columns,char **f_value,char **f_name){
 }
 int main(int argc, char const *argv[])
 {
-    sqlite3 *my_db;
-	if(sqlite3_open("shark.db",&my_db)!=SQLITE_OK){
-        printf("error code:%d\n",sqlite3_errcode(my_db));
-        printf("error:%s\n",sqlite3_errmsg(my_db));
-        exit(1);
-    }
-	if(3 != argc){
+    	sqlite3 *my_db;
+	if(sqlite3_open("shark.db",&my_db)!=SQLITE_OK)
+	{
+        	printf("error code:%d\n",sqlite3_errcode(my_db));
+       		printf("error:%s\n",sqlite3_errmsg(my_db));
+        	exit(1);
+	}
+	if(3 != argc)
+	{
 		printf("Usage : %s <ip> <port>\n", argv[0]);
 		exit(1);
 	}
@@ -63,40 +65,43 @@ int main(int argc, char const *argv[])
 		ERR_LOG("bind error");
 	if(-1 == listen(sockfd, 5))
 		ERR_LOG("listen error");
-    fd_set  readfds;
-    memset(&readfds,0,sizeof(readfds));
-    fd_set readfds_temp;
-    memset(&readfds_temp,0,sizeof(readfds_temp));
-    FD_SET(sockfd,&readfds);
+	fd_set  readfds;
+    	memset(&readfds,0,sizeof(readfds));
+    	fd_set readfds_temp;
+    	memset(&readfds_temp,0,sizeof(readfds_temp));
+    	FD_SET(sockfd,&readfds);
 	int max_fd=0;
 	max_fd=max_fd > sockfd ? max_fd : sockfd ;
 	int i=0;
 	int ret=0;
 	int acceptfd;
 	while(1){
-        readfds_temp=readfds;
+ 	        readfds_temp=readfds;
 		select(max_fd+1,&readfds_temp,NULL,NULL,NULL);
-		for(i=3;i<max_fd+1;i++){
-			if(FD_ISSET(i,&readfds_temp)){
-				if(sockfd==i){
+		for(i=3;i<max_fd+1;i++)
+		{
+			if(FD_ISSET(i,&readfds_temp))
+			{
+				if(sockfd==i)
+				{
 					acceptfd=accept(i,NULL,NULL);
 					printf("用户连接成功\n");
 					FD_SET(acceptfd,&readfds);
 					max_fd=max_fd > acceptfd ? max_fd : acceptfd ;
 					continue;
 				}
-				else {
-				ret=recv(i,&msg,sizeof(msg),0);
-				if(0==ret){
-					printf("用户断开连接\n");
-					FD_CLR(i,&readfds);
-					close(i);
-					continue;
-				}
-				printf("[%c][%s][%s]\n",msg.option,msg.name,msg.text);
-				printf("test\n");
-				printf("[%c][%s][%s]\n",msg.option,msg.name,msg.text);
-					switch(msg.option){
+				else 
+				{
+					ret=recv(i,&msg,sizeof(msg),0);
+					if(0==ret)
+					{
+						printf("用户断开连接\n");
+						FD_CLR(i,&readfds);
+						close(i);
+						continue;
+					}
+					switch(msg.option)
+					{
 						case '1':
 						do_register(i,&msg,my_db);
 						break;
@@ -112,27 +117,30 @@ int main(int argc, char const *argv[])
 				}
 			}
 		}
-    }
-    printf("test2\n");
+	}
 	return 0;
 }
 
-int do_register(int sockfd,msg_t *msg,sqlite3 *db){
+int do_register(int sockfd,msg_t *msg,sqlite3 *db)
+{
 	int ret=0;
 	char sqlbuff[350]={0};
 	sprintf(sqlbuff,"insert into user values( '%s' , '%s' , '%d' )",msg->name,msg->text,0);
-	if(SQLITE_OK!=sqlite3_exec(db,sqlbuff,NULL,NULL,NULL)){
-		if(19==sqlite3_errcode(db)){
+	if(SQLITE_OK!=sqlite3_exec(db,sqlbuff,NULL,NULL,NULL))
+	{
+		if(19==sqlite3_errcode(db))
+		{
 			sprintf(msg->text,"用户名[%s]已经被注册过了,请重新输入...",msg->name);
 			send(sockfd,msg,sizeof(msg_t),0);
-	}
+		}
 	}
 	sprintf(msg->text,"用户[%s]注册成功",msg->name);
 	send(sockfd,msg,sizeof(msg_t),0);
 	return 0;
 }
 
-int save(int sockfd,sqlite3 *db,int *money,msg_t *msg){
+int save(int sockfd,sqlite3 *db,int *money,msg_t *msg)
+{
 	int now_money=*money;
 	int save_money=0;
 	char save[300]={0};
@@ -157,8 +165,8 @@ int save(int sockfd,sqlite3 *db,int *money,msg_t *msg){
 	return 0;
 }
 
-int draw(int sockfd,sqlite3 *db,int *money,msg_t *msg){
-	printf("我是取\n");
+int draw(int sockfd,sqlite3 *db,int *money,msg_t *msg)
+{
 	int now_money=*money;
 	char draw[300]={0};
 	sprintf(draw,"-%s",msg->text);
@@ -177,62 +185,68 @@ int draw(int sockfd,sqlite3 *db,int *money,msg_t *msg){
 	char time_buff[128]={0};
 	struct tm *now_t=localtime(&now_time);
 	sprintf(time_buff,"[%d/%d/%d--%d:%d:%d]",now_t->tm_year+1900,now_t->tm_mon+1,now_t->tm_mday,\
-	    now_t->tm_hour,now_t->tm_min,now_t->tm_sec);
+	now_t->tm_hour,now_t->tm_min,now_t->tm_sec);
 	sprintf(sqlbuff,"insert into record values('%s','%s','%s' )",msg->name,time_buff,draw);
 	sqlite3_exec(db,sqlbuff,NULL,NULL,NULL);
 	return 0;
 }
-int do_login(int sockfd,msg_t *msg,sqlite3 *db){
-    char sqlbuff[350]={0};
+int do_login(int sockfd,msg_t *msg,sqlite3 *db)
+{
+    	char sqlbuff[350]={0};
 	num=0;
 	sprintf(sqlbuff,"select * from user where name = '%s'",msg->name);
 	sqlite3_exec(db,sqlbuff,login_func,NULL,NULL);
-    if(1==num){
-	num=0;
-    sprintf(sqlbuff,"select * from user where name = '%s' and password ='%s'",msg->name,msg->text);
-    sqlite3_exec(db,sqlbuff,login_func,NULL,NULL);
-    if(1==num){
-        sprintf(msg->text,"用户[%s]登录成功",msg->name);
-        send(sockfd,msg,sizeof(msg_t),0);
-    }else if(0==num){
-        sprintf(msg->text,"用户[%s]密码错误,请重新输入",msg->name);
-        send(sockfd,msg,sizeof(msg_t),0);
+    	if(1==num)
+	{
+		num=0;
+    		sprintf(sqlbuff,"select * from user where name = '%s' and password ='%s'",msg->name,msg->text);
+    		sqlite3_exec(db,sqlbuff,login_func,NULL,NULL);
+    		if(1==num)
+		{
+        		sprintf(msg->text,"用户[%s]登录成功",msg->name);
+        		send(sockfd,msg,sizeof(msg_t),0);
+    		}else if(0==num)
+		{
+        		sprintf(msg->text,"用户[%s]密码错误,请重新输入",msg->name);
+        		send(sockfd,msg,sizeof(msg_t),0);
+			return -1;
+    		}
+    	}else if(0==num)
+	{
+        	sprintf(msg->text,"用户[%s]尚未注册，请先注册后再尝试登录",msg->name);
+        	send(sockfd,msg,sizeof(msg_t),0);
 		return -1;
-    }
-    }else if(0==num){
-        sprintf(msg->text,"用户[%s]尚未注册，请先注册后再尝试登录",msg->name);
-        send(sockfd,msg,sizeof(msg_t),0);
+    	}
+	while(1)
+	{
+		if(0==(recv(sockfd,msg,sizeof(msg_t),0)))
 		return -1;
-    }
-	printf("test1\n");
-	while(1){
-	if(0==(recv(sockfd,msg,sizeof(msg_t),0)))
-		return -1;
-	switch(msg->option){
-		case '1':
-		save(sockfd,db,&money,msg);
-		break;
-		case '2':
-		printf("[%c][%s][%s]\n",msg->option,msg->name,msg->text);
-		draw(sockfd,db,&money,msg);
-		break;
-		case '3':
-		query_cmoney(sockfd,msg);
-		break;
-		case '4':
-		do_record(sockfd,msg,db);
-		break;
+		switch(msg->option)
+		{
+			case '1':
+			save(sockfd,db,&money,msg);
+			break;
+			case '2':
+			printf("[%c][%s][%s]\n",msg->option,msg->name,msg->text);
+			draw(sockfd,db,&money,msg);
+			break;
+			case '3':
+			query_cmoney(sockfd,msg);
+			break;
+			case '4':
+			do_record(sockfd,msg,db);
+			break;
+		}
 	}
-	}
-	printf("test2\n");
 	return 0;
 }
-int query_cmoney(int sockfd,msg_t *msg){
+int query_cmoney(int sockfd,msg_t *msg)
+{
 	sprintf(msg->text,"查询成功,您当前的存款为[%d]",money);
 	send(sockfd,msg,sizeof(msg_t),0);
 }
-int do_record(int sockfd,msg_t *msg,sqlite3 *db){
-	printf("我是记录\n");
+int do_record(int sockfd,msg_t *msg,sqlite3 *db)
+{
 	printf("%s\n",msg->name);
 	char sqlbuff[350]={0};
 	sprintf(sqlbuff,"select * from record where name = '%s'",msg->name);
